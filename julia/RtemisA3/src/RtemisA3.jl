68e88848ee886c6316038e6ec55acc509504e4ca
Stathis Gennatas <gennatas@gmail.com>
2026-03-30 20:31:55 -0700

    do not export internals

--- julia/RtemisA3/src/RtemisA3.jl
+++ julia/RtemisA3/src/RtemisA3.jl
@@ -11,8 +11,7 @@ include("api.jl")
 
 export A3ValidationError, A3ParseError
 export A3Metadata, SiteEntry, RegionEntry, FlexEntry, VariantRecord, A3Annotations, A3
-export sort_dedup, sort_ranges, check_no_overlap, is_json_compatible
-export to_dict, a3_from_json, a3_to_json, read_a3json, write_a3json
+export a3_from_json, a3_to_json, read_a3json, write_a3json
 export create_a3, residue_at, variants_at
 
 end # module RtemisA3
