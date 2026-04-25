90b4b4c0e197b71d63eb42bd0f59910a92e12bba
Stathis Gennatas <gennatas@gmail.com>
2026-04-24 19:58:48 -0700

    add check fns in upcoming rtemis.core

--- r/R/0_init.R
+++ r/R/0_init.R
@@ -49,3 +49,60 @@ clean_int <- function(x, xname = deparse(substitute(x))) {
   }
   cli::cli_abort("{.var {xname}} must be integer.")
 }
+
+
+# %% check_optional_scalar_character ----
+#' Check Optional Scalar Character
+#'
+#' @param x Optional Character: Value to check.
+#' @param arg_name Character: Argument name to use in error messages.
+#'
+#' @return Called for side effects.
+#'
+#' @author EDG
+#' @keywords internal
+#' @noRd
+#'
+#' @examples
+#' check_optional_scalar_character(NULL, "my_arg") # Passes
+#' check_optional_scalar_character("hello", "my_arg") # Passes
+#' # Throw error:
+#' try(check_optional_scalar_character(c("hello", "world"), "my_arg"))
+#' try(check_optional_scalar_character(123, "my_arg"))
+check_optional_scalar_character <- function(
+  x,
+  arg_name = deparse(substitute(x))
+) {
+  check_character(x, allow_null = TRUE, arg_name = arg_name)
+  if (!is.null(x) && length(x) != 1L) {
+    cli::cli_abort(
+      "{.var {arg_name}} must be NULL or a single string."
+    )
+  }
+  invisible()
+}
+
+
+#' Check Scalar Character
+#'
+#' @param x Character: Value to check.
+#' @param arg_name Character: Argument name to use in error messages.
+#'
+#' @return Called for side effects.
+#'
+#' @author EDG
+#' @keywords internal
+#' @noRd
+#'
+#' @examples
+#' check_scalar_character("hello", "my_arg") # Passes
+#' # Throw error:
+#' try(check_scalar_character(c("hello", "world"), "my_arg"))
+#' try(check_scalar_character(123, "my_arg"))
+check_scalar_character <- function(x, arg_name) {
+  check_character(x, allow_null = FALSE, arg_name = arg_name)
+  if (length(x) != 1L) {
+    cli::cli_abort("{.var {arg_name}} must be a single string.")
+  }
+  invisible()
+}
